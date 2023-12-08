7538e76785bc73bc961e0872fc2ad7bf9085645f
天南十字星 <zjh96@live.cn>
2023-12-08 21:48:22 +0800

    Add CLI arguments parsing and help info.

--- TeeSharp/Program.cs
+++ TeeSharp/Program.cs
@@ -1,8 +1,26 @@
-﻿var files = args;
+﻿var cmdOpts = CommandOptions.Parse(args);
+if (cmdOpts.InvalidOption != null)
+{
+    foreach (var line in GetInvalidOptionMessage(cmdOpts.InvalidOption))
+    {
+        Console.Error.WriteLine(line);
+    }
+    return 1;
+}
+if (cmdOpts.Help)
+{
+    foreach (var line in GetHelpMessage())
+    {
+        Console.Out.WriteLine(line);
+    }
+    return 0;
+}
+
 var stdin = Console.OpenStandardInput();
 var stdout = Console.OpenStandardOutput();
-var streams = Array.ConvertAll(files, file => new FileStream(
-    file, FileMode.Create, FileAccess.Write, FileShare.ReadWrite));
+var fileMode = cmdOpts.Append ? FileMode.Append : FileMode.Create;
+var streams = Array.ConvertAll(cmdOpts.Files, file => (file == "-") ? stdout :
+    new FileStream(file, fileMode, FileAccess.Write, FileShare.ReadWrite));
 
 const int bufferSize = 4096;
 var readBuffer = new byte[bufferSize];
@@ -31,3 +49,74 @@ foreach (var stream in streams)
 }
 stdout.Dispose();
 stdin.Dispose();
+return 0;
+
+static IEnumerable<string> GetHelpMessage()
+{
+    var cmdName = Program.GetCommandName();
+    // Don't use the multi-line string literal to avoid hard-coding the newline sequence.
+    yield return $"Usage: {cmdName} [OPTION]... [FILE]...";
+    yield return $"Copy standard input to each FILE, and also to standard output.";
+    yield return "";
+    yield return "    -a, --append         Append to the given FILEs, do not overwrite.";
+    yield return "    -?, -h, --help       Display this help and exit.";
+    yield return "";
+    yield return "When FILE is -, copy again to standard output.";
+}
+
+static IEnumerable<string> GetInvalidOptionMessage(string option)
+{
+    var cmdName = Program.GetCommandName();
+    yield return $"Invalid option: {option}";
+    yield return $"Try '{cmdName} --help' for more information.";
+}
+
+static partial class Program
+{
+    internal static string GetCommandName()
+    {
+        var cmdPath = Environment.GetCommandLineArgs()[0];
+        var cmdName = Path.GetFileNameWithoutExtension(cmdPath);
+        var cmdExt = Path.GetExtension(cmdPath);
+        var hasPathExt = Environment.OSVersion.Platform < PlatformID.Unix;
+        return (hasPathExt && (cmdExt.Length > 0)) ?
+            $"{cmdName}[{cmdExt}]" : Path.GetFileName(cmdPath);
+    }
+}
+
+readonly record struct CommandOptions(bool Help, bool Append, string[] Files)
+{
+    public string? InvalidOption { get; init; }
+
+    public static CommandOptions Parse(string[] args)
+    {
+        var help = false;
+        var append = false;
+        var files = new List<string>(args.Length);
+        var invalidOpt = default(string);
+        foreach (var index in ..args.Length)
+        {
+            var arg = args[index];
+            switch (arg)
+            {
+                case "-?" or "-h" or "--help":
+                    help = true;
+                    break;
+                case "-a" or "--append":
+                    append = true;
+                    break;
+                case "--":
+                    files.AddRange(args[(index + 1)..]);
+                    goto ParseEnd;
+                case { Length: > 1 } when arg.StartsWith('-'):
+                    invalidOpt = arg;
+                    goto ParseEnd;
+                default:
+                    files.Add(arg);
+                    break;
+            }
+        }
+    ParseEnd:
+        return new(help, append, files.ToArray()) { InvalidOption = invalidOpt };
+    }
+}
