9a27e416795ef8bb2a41a8b2c29492112a9ccaae
天南十字星 <zjh96@live.cn>
2023-12-11 21:33:41 +0800

    Add basic implementation in F# of the tee tool.

--- TeeFSharp/Program.fs
+++ TeeFSharp/Program.fs
@@ -1,2 +1,32 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.IO
+open System.Threading.Tasks
+
+let files = Environment.GetCommandLineArgs()[1..]
+let stdin = Console.OpenStandardInput()
+let stdout = Console.OpenStandardOutput()
+let streams = files |> Array.map (fun file -> new FileStream(
+    file, FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+
+let rec copyInput (buffer: byte[]) (lastBuffer: byte[])
+                  (lastStdoutTask: Task) (lastStreamTasks: Task[]) =
+    let lengthTask = task {
+        let! length = stdin.ReadAsync(buffer)
+        let! _ = lastStdoutTask
+        let! _ = Task.WhenAll(lastStreamTasks)
+        return length
+    }
+    match lengthTask.Result with
+    | 0 -> ()
+    | length ->
+        let streamTasks = streams |> Array.map _.WriteAsync(buffer, 0, length)
+        let stdoutTask = stdout.WriteAsync(buffer, 0, length)
+        copyInput lastBuffer buffer stdoutTask streamTasks
+
+let [<Literal>] bufferSize = 4096
+copyInput (Array.zeroCreate bufferSize) (Array.zeroCreate bufferSize)
+          (Task.CompletedTask) (streams |> Array.map (fun _ -> Task.CompletedTask))
+
+streams |> Array.iter (_.Dispose())
+stdout.Dispose()
+stdin.Dispose()
